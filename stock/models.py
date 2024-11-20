3b424edcd00129fbac55cfb73f855bb80d45093e
esteban-florez <eflorez077@gmail.com>
2024-11-20 15:02:18 -0400

    Creación de modelos y migración.

--- stock/models.py
+++ stock/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class TimeStampedModel(models.Model):
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+  class Meta:
+      abstract = True
+
+class Category(TimeStampedModel):
+  name = models.CharField(max_length=20)
+  description = models.CharField(max_length=255)
+
+class Product(TimeStampedModel):
+  name = models.CharField(max_length=30)
+  description = models.CharField(max_length=255)
+  price = models.FloatField()
+  category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class Supplier(TimeStampedModel):
+  name = models.CharField(max_length=30)
+  description = models.CharField(max_length=255)
+
+class Movement(TimeStampedModel):
+  amount = models.IntegerField()
+  reason = models.CharField(max_length=30, null=True)
+  product = models.ForeignKey(Product, on_delete=models.CASCADE)
+  supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
