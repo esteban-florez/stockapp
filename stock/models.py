1fe0cb4dc80f38fdbd9b68a6186f6682fb015ff6
esteban-florez <eflorez077@gmail.com>
2024-11-21 12:02:53 -0400

    Crear vista de estado de inventario.

--- stock/models.py
+++ stock/models.py
@@ -11,18 +11,30 @@ class Category(TimeStampedModel):
   name = models.CharField(max_length=20)
   description = models.CharField(max_length=255)
 
+  def __str__(self):
+    return f"{self.name}"
+
 class Product(TimeStampedModel):
   name = models.CharField(max_length=30)
   description = models.CharField(max_length=255)
   price = models.FloatField()
   category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
+  def __str__(self):
+    return f"{self.name}"
+
 class Supplier(TimeStampedModel):
   name = models.CharField(max_length=30)
   description = models.CharField(max_length=255)
+  
+  def __str__(self):
+    return f"{self.name}"
 
 class Movement(TimeStampedModel):
   amount = models.IntegerField()
   reason = models.CharField(max_length=30, null=True)
   product = models.ForeignKey(Product, on_delete=models.CASCADE)
   supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE, null=True)
+
+  def __str__(self):
+    return f"{self.product.name} {self.amount}"
--- stock/views.py
+++ stock/views.py
@@ -1,7 +1,12 @@
+from .models import Product
+from django.db.models import Sum
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
+  products = Product.objects.order_by('-created_at').annotate(stock=Sum('movement__amount'))
+  links = ['Inicio', 'Estado de Inventario']
+  
   return render(request, 'index.html', {
-    'links': ['Inicio', 'Existencia de Inventario'],
+    'products': products,
+    'links': links
   })
