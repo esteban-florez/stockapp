90038fa56d0c90b1de594fdf1176d25294134c2a
esteban-florez <eflorez077@gmail.com>
2025-02-04 23:54:58 -0400

    Arreglar archivos estáticos y seeding

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
 
 echo "Seeding Database..."
-python3 manage.py seed --noinput
+python3 manage.py seed
 
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput
